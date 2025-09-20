2ec871fdb87e056964a1ea07e91b56eaf9991ad8
xfnw <xfnw@riseup.net>
2025-09-19 23:07:19 -0400

    emacs: shrink font
    wtf is my font stack just screwed? the font size grew itself until it
    borked my split width threshold

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -112,7 +112,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 105 :width normal :foundry "JB" :family "JetBrainsMono Nerd Font"))))
+ '(default ((t (:height 100 :width normal :foundry "JB" :family "JetBrainsMono Nerd Font"))))
  '(line-number ((t (:background nil :foreground "#7c6f64"))))
  '(line-number-current-line ((t (:background nil :foreground "#fe8019")))))
 
