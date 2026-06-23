751ec36e3d0032dbbd4cf25bd43a04c5eec165cb
xfnw <xfnw@riseup.net>
2026-06-23 11:12:46 -0400

    emacs: migrate company to use-package

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -311,24 +311,26 @@ Directory defaults to the value of `move-file-default-target'."
 (add-hook 'lisp-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook #'enable-paredit-mode)
 
-(require 'company)
-(define-key company-mode-map (kbd "C-<tab>") #'company-complete)
-(define-key company-mode-map (kbd "<backtab>") #'company-complete)
-(define-key company-active-map (kbd "C-l") #'company-abort)
-(define-key company-active-map (kbd "C-<up>") #'company-abort)
-(define-key company-active-map (kbd "C-<down>") #'company-abort)
-(define-key company-active-map (kbd "C-<left>") #'company-abort)
-(define-key company-active-map (kbd "C-<right>") #'company-abort)
-(define-key company-active-map (kbd "<escape>") #'company-abort)
-(define-key company-active-map (kbd "C-j") #'company-select-next-or-abort)
-(define-key company-active-map (kbd "C-k") #'company-select-previous-or-abort)
-
-(defun turn-off-company ()
-  (company-mode -1))
-(add-hook 'term-mode-hook #'turn-off-company)
-(add-hook 'eshell-mode-hook #'turn-off-company)
-(add-hook 'circe-mode-hook #'turn-off-company)
-(add-hook 'org-mode-hook #'turn-off-company)
+(use-package company
+  :bind (:map company-mode-map
+         ("C-<tab>" . company-complete)
+         ("<backtab>" . company-complete)
+         :map company-active-map
+         ("C-l" . company-abort)
+         ("C-<up>" . company-abort)
+         ("C-<down>" . company-abort)
+         ("C-<left>" . company-abort)
+         ("C-<right>" . company-abort)
+         ("<escape>" . company-abort)
+         ("C-j" . company-select-next-or-abort)
+         ("C-k" . company-select-previous-or-abort))
+  :config
+  (defun turn-off-company ()
+    (company-mode -1))
+  (add-hook 'term-mode-hook #'turn-off-company)
+  (add-hook 'eshell-mode-hook #'turn-off-company)
+  (add-hook 'circe-mode-hook #'turn-off-company)
+  (add-hook 'org-mode-hook #'turn-off-company))
 
 (add-hook 'doc-view-mode-hook #'auto-revert-mode)
 
